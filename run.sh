d07ebefa534b7f0f80f8a9ed7f5d6b0a000f0430
Win <winsdominoes@winscloud.net>
2026-03-31 22:43:36 +0700

    Added some changelogs / notes - Works well!

--- run.sh
+++ run.sh
@@ -11,12 +11,10 @@ CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fuse-ld=lld
 # disable unnecessary stack protection, no std library
 
 # build kernel
-$CC $CFLAGS -Wl, -Tkernel.ld -Wl, -Map=kernel.map -o kernel.elf \ 
-    kernel.c
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
 # -Wl => passing options to linker (and not C compiler)
 # -Wl, -Tkernel.ld: specify linker script
 # -Wl, -Map=kernel.map: Output a map file (linker allocation result)
 
 # QEMU start cmd
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
-    -kernel kernel.elf
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf

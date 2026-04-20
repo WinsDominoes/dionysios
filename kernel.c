dfa78c0405396f5174c85ec50f6b5963fb995517
Win <winsdominoes@winscloud.net>
2026-04-20 16:07:16 +0700

    Added scheduler - yield() function

--- kernel.c
+++ kernel.c
@@ -84,7 +84,125 @@ void handle_trap(struct trap_frame *f)
     PANIC("unexpected trap scause=%x, stval=%x, sepc=%x\n", scause, stval, user_pc);
 }
 
+// Process creation
+struct process procs[PROCS_MAX];             // All process control structures
+
+struct process *create_process(uint32_t pc)
+{
+    // Find unused proc in control structure.
+    struct process *proc = NULL;
+    int i;
+    for (i = 0; i < PROCS_MAX; i++)
+    {
+        if (procs[i].state == PROC_UNUSED)
+        {
+            proc = &procs[i];
+            break;
+        }
+    }
+
+    if (!proc)
+    {
+        PANIC("No free process slots");
+    }
+
+    // Stack callee-saved registers. 
+    uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+    *--sp = 0;                      // s11
+    *--sp = 0;                      // s10
+    *--sp = 0;                      // s9
+    *--sp = 0;                      // s8
+    *--sp = 0;                      // s7
+    *--sp = 0;                      // s6
+    *--sp = 0;                      // s5
+    *--sp = 0;                      // s4
+    *--sp = 0;                      // s3
+    *--sp = 0;                      // s2
+    *--sp = 0;                      // s1
+    *--sp = 0;                      // s0
+    *--sp = (uint32_t) pc;          // ra
+
+    // Initialize fields.
+    proc->pid = i + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+}
+
 // CONTEXT SWITCHING
+__attribute__((naked)) void switch_context(uint32_t *prev_sp,
+                                           uint32_t *next_sp)
+{
+    __asm__ __volatile__
+    (
+        // Callee-saved registers: Registers that a called function must restore before returning.
+        // Save callee-saved registers onto the current process's stack.
+        "addi sp, sp, -13 * 4\n" // Allocate stack space for 13 4-byte registers
+        "sw ra,  0  * 4(sp)\n"   // Save callee-saved registers only
+        "sw s0,  1  * 4(sp)\n"
+        "sw s1,  2  * 4(sp)\n"
+        "sw s2,  3  * 4(sp)\n"
+        "sw s3,  4  * 4(sp)\n"
+        "sw s4,  5  * 4(sp)\n"
+        "sw s5,  6  * 4(sp)\n"
+        "sw s6,  7  * 4(sp)\n"
+        "sw s7,  8  * 4(sp)\n"
+        "sw s8,  9  * 4(sp)\n"
+        "sw s9,  10 * 4(sp)\n"
+        "sw s10, 11 * 4(sp)\n"
+        "sw s11, 12 * 4(sp)\n"
+
+        // Switch the stack pointer.
+        "sw sp, (a0)\n"         // *prev_sp = sp;
+        "lw sp, (a1)\n"         // Switch stack pointer (sp) here
+
+        // Restore callee-saved registers from the next process's stack.
+        "lw ra,  0  * 4(sp)\n"  // Restore callee-saved registers only
+        "lw s0,  1  * 4(sp)\n"
+        "lw s1,  2  * 4(sp)\n"
+        "lw s2,  3  * 4(sp)\n"
+        "lw s3,  4  * 4(sp)\n"
+        "lw s4,  5  * 4(sp)\n"
+        "lw s5,  6  * 4(sp)\n"
+        "lw s6,  7  * 4(sp)\n"
+        "lw s7,  8  * 4(sp)\n"
+        "lw s8,  9  * 4(sp)\n"
+        "lw s9,  10 * 4(sp)\n"
+        "lw s10, 11 * 4(sp)\n"
+        "lw s11, 12 * 4(sp)\n"
+        "addi sp, sp, 13 * 4\n"  // We've popped 13 4-byte registers from the stack
+        "ret\n"
+    );
+}
+
+// Scheduler
+struct process *current_proc;                   // Current process dat is running
+struct process *idle_proc;                      // idle process - the process to run when there are no runnable processes
+
+void yield(void)
+{
+    // Search for runnable process
+    struct process *next = idle_proc;
+    for (int i = 0; i < PROCS_MAX; i++)
+    {
+        struct process *proc = &procs[(current_proc->pid + i)];
+        if (proc->state == PROC_RUNNABLE && proc->pid > 0)
+        {
+            next = proc;
+            break;
+        }
+    }
+
+    // If no runnable proc, continue running this current proc
+    if (next == current_proc)
+        return;
+
+    // context switching
+    struct process *prev = current_proc;
+    current_proc = next; 
+    switch_context(&prev->sp, &next->sp);
+}
+
 
 __attribute__((naked))                      // tells compiler -> no generating unnecessary code before and after function
 __attribute__((aligned(4)))                 // aligns function starting addr to a 4-byte boundary
@@ -173,25 +291,74 @@ void kernel_entry(void)
     );
 }
 
+// Test context switching
+void delay(void)
+{
+    for (int i = 0; i < 30000000; i++)
+    {
+        __asm__ __volatile__("nop");        // do nothing
+    }
+}
+
+struct process *proc_a;
+struct process *proc_b;
+
+// Process A
+void proc_a_entry(void)
+{
+    printf("Starting process A\n");
+    while (1)
+    {
+        putchar('A');
+        switch_context(&proc_a->sp, &proc_b->sp);
+        yield();
+    }
+}
+
+// Proces B
+void proc_b_entry(void)
+{
+    printf("Start process B\n");
+    while (1)
+    {
+        putchar('B');
+        switch_context(&proc_b->sp, &proc_a->sp);
+        yield();
+    }
+}
+
 // Main Kernel function
 void kernel_main(void)
 {
     memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);  // initializes / sets the bss section to zero
 
-	paddr_t paddr0 = alloc_pages(2);		// Alloc two pages of memory (8KB)
-	paddr_t paddr1 = alloc_pages(1);		// Alloc one page of memory	(4KB)
-	printf("alloc_pages test: paddr0=%x\n", paddr0);
-	printf("alloc_pages test: paddr1=%x\n", paddr1);
+    printf("\n\n");
+    
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
+    
+    idle_proc = create_process((uint32_t) NULL);
+    idle_proc->pid = 0;                     // Idle process id
+    current_proc = idle_proc;
+
+    proc_a = create_process((uint32_t) proc_a_entry);
+    proc_b = create_process((uint32_t) proc_b_entry);
+
+    yield();
+    PANIC("Switched to idle process");
+
+	// paddr_t paddr0 = alloc_pages(2);		// Alloc two pages of memory (8KB)
+	// paddr_t paddr1 = alloc_pages(1);		// Alloc one page of memory	(4KB)
+	// printf("alloc_pages test: paddr0=%x\n", paddr0);
+	// printf("alloc_pages test: paddr1=%x\n", paddr1);
 
     // printf("\n\nHello %s\n", "World!");
     // printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
 
-	PANIC("booted!");
+	// PANIC("unreachable here!");
 
 
     /* UNUSED 
     
-    WRITE_CSR(stvec, (uint32_t) kernel_entry);
     // unimp is a pseudo instruction - the assembler turns it into
     // csrrw x0, cycle, x0 
     // reads and writes the cycle register into x0 
